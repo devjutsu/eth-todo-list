b6a00de463b87a4d6cfecc9155e46a4f04a06cb4
Albert Garuda <algarud@gmail.com>
2022-01-27 20:10:38 +0200

    Enabling metamask

--- src/app.js
+++ src/app.js
@@ -3,50 +3,39 @@ App = {
     load: async () => {
         console.log("app loading...");
         await App.loadWeb3();
-        
-        
-        // console.log(web3.eth.accounts);
+        await App.loadAccount();
+        await App.loadContract();
+        await App.render();
     },
     
     loadWeb3: async () => {
-        const provider = await detectEthereumProvider();
+        App.provider = await detectEthereumProvider();
         
-        if (provider) {
+        if (App.provider) {
             console.log('Ethereum successfully detected!')
 
-            const chainId = await provider.request({
+            const chainId = await App.provider.request({
               method: 'eth_chainId'
             });
-            
+            console.log(chainId);
+
+            await ethereum.enable();
+
           } else {
             console.error('Please install MetaMask!', error)
           }
+    },
 
-        // if (typeof web3 !== 'undefined') {
+    loadAccount: async () => {
+        
+    },
 
-        //     App.web3Provider = web3.currentProvider
-        //     web3 = new Web3(web3.currentProvider)
-        // } else {
-        //     window.alert("Please, connect to Metamask");
-        // }
+    loadContract: async() => {
 
-        // if (window.ethereum) {
-        //     window.web3 = new Web3(ethereum);
-        //     try {
-        //         await ethereum.enable();
-        //         web3.eth.sendTransaction({});
-        //     } catch (error) {
+    },
 
-        //     }
-        // }
-        // else if (window.web3) {
-        //     App.web3Provider = web3.currentProvider;
-        //     window.web3 = new Web3(web3.currentProvider);
-        //     web3.eth.sendTransaction({});
-        // }
-        // else {
-        //     console.log("Non-Etherium browser detected. Add Metamask or smth.");
-        // }
+    render: async() => {
+        
     }
 }
 
